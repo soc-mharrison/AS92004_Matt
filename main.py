a33dddf8cb39301c48224bf69ec72782921af58f
soc-mharrison <137470944+soc-mharrison@users.noreply.github.com>
2025-04-29 14:16:35 +1200

    Create main.py
    Added comments, setup variables, ask a question

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+Week 6 Quiz
+by Matt Harrison
+Date: Tues 6.03.2025
+Version 1
+'''
+# setup variables
+msg = "What is the kupu for: "
+score = 0
+# list index : 0, 1, 2
+questions = ["one","two","three"]
+answers = ["tahi","rua","toru"]
+
+print("""Harae Mai! \nWelcome to the \nTe Reo Māori quiz""")
+# ask Qs
+i = 0
+while i < len(questions):
+  user_answer = input(msg, questions[i])
+  i += 1
+# check if ans == answer[i]
+
+# calc score
+
+
+
+
+
+
