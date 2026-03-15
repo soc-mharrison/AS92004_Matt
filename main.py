805ca90c575b7038283b52e3159a5d8a4bae0f54
soc-mharrison <137470944+soc-mharrison@users.noreply.github.com>
2026-03-15 19:00:46 +1300

    Update main.py
    Removed exemplar and made as generic

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 """
-Week 6 Quiz
-by Matt Harrison
-Date: Tues 6.03.2025
-Version 1
+Docstring TITLE
+by AUTHOR
+Date: Day dd/mm/yy
+Version n?
 """
 # setup variables
-msg = "What is the kupu for: "
-score = 0
-# list index : 0, 1, 2
-questions = ["one","two","three"]
-answers = ["tahi","rua","toru"]
 
-print("""Harae Mai! \nWelcome to the \nTe Reo Māori quiz""")
-# ask Qs
-i = 0
-while i < len(questions):
-  user_answer = input(msg, questions[i])
-  i += 1
-# check if ans == answer[i]
 
-# calc score
+# Setup data structures
+
+# Declare Functions
+
+####################
+# Main loop
+###################
+
+# Take input
+
+# Calculate
+
+# Display Output
 
 
 
